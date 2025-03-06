977ff6187b1e6c7778f2bc5026f7b02a79a7ac02
Bhawna Pal <bhawnapal2104@gmail.com>
2025-03-06 22:07:04 +0530

    Commit

---
+++ 03_Basics/03_arrow.js
@@ -0,0 +1,48 @@
+const user = {
+    username : "Bhawna",
+    price : 999,
+
+    welcomeMessage : function() {
+        console.log(`${this.username} , Welcome to Website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "Sam"
+// user.welcomeMessage()
+
+// console.log(this)
+
+// function chai() {
+//     let username = "Bhawna"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function() {
+//     let username = "Bhawna"
+//     console.log(this.username);
+// }
+
+// const chai =  () => {
+//     let username = "Bhawna"
+//     console.log(this);
+// }
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+//     } 
+
+// const addTwo = (num1, num2) => num1 + num2
+
+// const addTwo = (num1, num2) => (num1 + num2)
+
+const addTwo = (num1, num2) => ({username: "Bhawna"})
+
+console.log(addTwo(3, 4))
