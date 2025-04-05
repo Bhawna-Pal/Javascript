f871a73cf4fcb44f1247597743b4e80714c8eac8
Bhawna Pal <bhawnapal2104@gmail.com>
2025-04-05 17:12:18 +0530

    commit

--- 07_projects/BMI Calculator/javascript.js
+++ 07_projects/BMI Calculator/javascript.js
@@ -23,15 +23,5 @@ form.addEventListener('submit', function(e){
   };
 
 
-//   const bmiguide = document.querySelector('#bmiguide')
-//   const bmiresult = document.querySelector('#result')
-//     if (bmiresult <= 18.6) {
-//       bmiguide.innerHTML = `You are underweight `
-// } else if (bmiresult > 18.6 && bmiresult < 24.9 ){
-//       bmiguide.innerHTML = `you are Normal range `
-//  } else if (bmiresult >= 24.9){
-//       bmiguide.innerHTML = `You are Overweight `
-// }
-//     bmiresult.innerHTML = `<span>${bmiguide} </span>`
-    
+
 });
